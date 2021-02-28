bf0cfe871639a6ae8a3b4c72829e7213c69543aa
Lisa Pessin <lisapessin@gmail.com>
2021-02-28 16:06:27 -0500

    initial commit

---
+++ script.js
@@ -0,0 +1,21 @@
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
+
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
+// length 8 - 128 characters
+// include upper case
+// include lowercase
+// include number
+// include special character
