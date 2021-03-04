8db82b556742f116a6c164ded9d1d819d1b9a519
Lisa Pessin <lisapessin@gmail.com>
2021-03-04 12:07:36 -0500

    full code, generates password, problem with 1 alert message

--- script.js
+++ script.js
@@ -1,24 +1,3 @@
-// Assignment Code
-var generateBtn = document.querySelector("#generate");
-
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-
-
-
-
-
-
-
 
 // Array for upper case
 var upperCase = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
@@ -36,6 +15,10 @@ var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 var specialCharacter = [" ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "'", "-", "+", "=", "{", "}", "[", "]", "|", "~", "`", "_", ";", ":", "<", ">", ",", ".", "?", "/"];
 
 
+// // Array for everything that is not a numnber
+// var notNumber = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z", "a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", " ", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "'", "-", "+", "=", "{", "}", "[", "]", "|", "~", "`", "_", ";", ":", "<", ">", ",", ".", "?", "/"] 
+
+
 
 
 // User prompt for password length 8-128, options for character types
@@ -44,17 +27,20 @@ function generatePassword() {
     prompt("How many characters in your password? Choose between 8-128.")
     );
 
-    // if input is <8, error message
-    if (length < 8) {
-      prompt("Password must contain at least 8 characters.");
+    
+
+    // if input is <8 and >128, error message
+    if (length < 8 || length > 128) {
+      alert("Password must be between 8 and 128 characters.");
       return;
     }
 
-    // if input is >128, error message
-    if (length > 128) {
-      prompt("Password must contain less than 129 characters.");
-      return;
-    }
+    // else if (length = upperCase && lowerCase && specialCharacter) {
+    //   alert("Length must be entered as a number.")
+    //   return;
+    // }
+
+    
 
       // contain character type upperCase? BOOLEAN
       var confirmUpperCase = confirm("Will your password contain upper case letters?");
@@ -66,40 +52,120 @@ function generatePassword() {
       var confirmSpecialCharacter = confirm("Will your password contain special characters?");
     
 
-// if no character type chosen, error message
+    // if no character type chosen, error message
     if (!confirmUpperCase && !confirmLowerCase && !confirmNumber && !confirmSpecialCharacter) {
-      prompt("Password must contain some character type.");
+      alert("Password must contain some character type.");
       return;
     }
 
    
+
+
+  // else if for all 4 options chosen
+    else if (confirmUpperCase && confirmLowerCase && confirmNumber && confirmSpecialCharacter) {
+      choices = upperCase.concat(lowerCase, number, specialCharacter);
+    }
+
+    // else if for 3 options chosen
+    else if (confirmUpperCase && confirmLowerCase && confirmNumber) {
+      choices = upperCase.concat(lowerCase, number);
+    }
+
+    else if (confirmUpperCase && confirmLowerCase && confirmSpecialCharacter) {
+      choices = upperCase.concat(lowerCase, specialCharacter);
+    }
+
+    else if (confirmUpperCase && confirmNumber && confirmSpecialCharacter) {
+      choices = upperCase.concat(number, specialCharacter);
+    }
+
+    else if (confirmLowerCase && confirmNumber && confirmSpecialCharacter) {
+      choices = lowerCase.concat(number, specialCharacter);
+    }
+
+    // else if for 2 options chosen
+    else if (confirmUpperCase && confirmLowerCase) {
+      choices = upperCase.concat(lowerCase);
+    }
+
+    else if (confirmUpperCase && confirmNumber) {
+      choices = upperCase.concat(number);
+    }
+
+    else if (confirmUpperCase && confirmSpecialCharacter) {
+      choices = upperCase.concat(specialCharacter);
+    }
+
+    else if (confirmLowerCase && confirmNumber) {
+      choices = lowerCase.concat(number);
+    }
+
+    else if (confirmLowerCase && confirmSpecialCharacter) {
+      choices = lowerCase.concat(specialCharacter);
+    }
+
+    else if (confirmNumber && confirmSpecialCharacter) {
+      choices = number.concat(specialCharacter);
+    }
+
+    // else if for 1 option chosen
+    else if (confirmUpperCase) {
+      choices = upperCase;
+    }
+
+    else if (confirmLowerCase) {
+      choices = lowerCase;
+    }
+
+    else if (confirmNumber) {
+      choices = number;
+    }
+
+    else if (confirmSpecialCharacter) {
+      choices = specialCharacter;
+    }
+
+
+    // var password is array placeholder
+    var password = [];
+
+
+    // for loop to select random characters
+    for (var i = 0; i < length; i++) {
+      var userChoices = choices[Math.floor(Math.random() * choices.length)];
+      password.push(userChoices);
+    }
+
+
+    // copied from youTube, no idea what the .join("") means
+    var ps = password.join("");
+    userInput(ps);
+    return ps;
+
+}
+
+// // continuation of what I copied from youTube
+function userInput(ps) {
+  document.getElementById("password").textContent = ps;
 }
 
 
 
-// function to get random character from array
-// Math.floor ?
-// Math.random ?
 
+// Assignment Code
+var generateBtn = document.querySelector("#generate");
 
+// Write password to the #password input
+function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
 
-// function 
-// if includes upperCase
-// if includes lowerCase
-// if includes number
-// if includes specialCharacter
-// for loop inside function
-//        for (var i = 0, i < ???; i++) {}
+  passwordText.value = password;
 
+}
 
-
-
-
-
-
-
-
-
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
 
 
 
